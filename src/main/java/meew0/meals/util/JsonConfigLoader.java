7a8dc19ac07fccbb4ceabf55cea98a923bc2f5e4
meew0 <blactbt@live.de>
2014-05-29 13:06:29 +0200

    Aaand another one.

--- src/main/java/meew0/meals/util/JsonConfigLoader.java
+++ src/main/java/meew0/meals/util/JsonConfigLoader.java
@@ -131,8 +131,8 @@ public class JsonConfigLoader {
                 buffer.write("\t\t\"saturation\":\"0.2\",\n");
                 buffer.write("\t\t\"useDuration\":\"20\",\n");
                 buffer.write("\t\t\"texture\":\"minecraft:diamond\",\n");
-                buffer.write("\t\t\"recipe\":\"ccc,cic,ccc,c,coal,i,iron_ingot\"\n");
-                buffer.write("\t\t\"recipeAmount\":\"1\",\n");
+                buffer.write("\t\t\"recipe\":\"ccc,cic,ccc,c,coal,i,iron_ingot\",\n");
+                buffer.write("\t\t\"recipeAmount\":\"1\"\n");
                 buffer.write("\t},\n");
                 buffer.write("\t{\n");
                 buffer.write("\t\t\"name\":\"Test Food 2\",\n");
@@ -140,8 +140,8 @@ public class JsonConfigLoader {
                 buffer.write("\t\t\"saturation\":\"1.0\",\n");
                 buffer.write("\t\t\"useDuration\":\"100\"\n");
                 buffer.write("\t\t\"texture\":\"meals:test\",\n");
-                buffer.write("\t\t\"recipe\":\"ccc,cdc,sss,c,coal,d,dye:2,s,stone\"\n");
-                buffer.write("\t\t\"recipeAmount\":\"8\",\n");
+                buffer.write("\t\t\"recipe\":\"ccc,cdc,sss,c,coal,d,dye:2,s,stone\",\n");
+                buffer.write("\t\t\"recipeAmount\":\"8\"\n");
                 buffer.write("\t}\n");
                 buffer.write("]");
 
